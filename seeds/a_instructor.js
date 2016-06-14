b81cfea315739781f2a56dcb7b5c2a075710b1f2
lionel_lints <lionel.lints@gmail.com>
2016-06-14 10:06:38 -0700

    add seed instructor data

--- seeds/a_instructor.js
+++ seeds/a_instructor.js
@@ -1,12 +1,42 @@
 
-//exports.seed = function(knex, Promise) {
-  //return Promise.join(
-    //// Deletes ALL existing entries
-    //knex('table_name').del(),
+module.exports.seed = function(knex, Promise) {
+  return Promise.join(
+    knex('instructor').del(),
 
-    //// Inserts seed entries
-    //knex('table_name').insert({id: 1, colName: 'rowValue'}),
-    //knex('table_name').insert({id: 2, colName: 'rowValue2'}),
-    //knex('table_name').insert({id: 3, colName: 'rowValue3'})
-  //);
-//};
+    knex('instructor').insert({
+      id: 1,
+      email: 'cj.reynolds@galvanize.com',
+      first_name: 'CJ',
+      last_name: 'Reynolds',
+      DSI: false,
+      WDI: true,
+    }),
+
+    knex('instructor').insert({
+      id: 2,
+      email: 'lionel.lints@galvanize.com',
+      first_name: 'Lionel',
+      last_name: 'Lints',
+      DSI: false,
+      WDI: true,
+    }),
+
+    knex('instructor').insert({
+      id: 3,
+      email: 'chris.burkhart@galvanize.com',
+      first_name: 'Chris',
+      last_name: 'Burkhart',
+      DSI: false,
+      WDI: true,
+    }),
+
+    knex('instructor').insert({
+      id: 4,
+      email: 'adam.lichty@galvanize.com',
+      first_name: 'Adam',
+      last_name: 'Lichty',
+      DSI: false,
+      WDI: true,
+    })
+  );
+};
