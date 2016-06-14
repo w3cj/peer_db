ad6b8e06a943576f8dc97089bc1b0b63b04e4254
lionel_lints <lionel.lints@gmail.com>
2016-06-13 20:03:13 -0700

    add more join tables

---
+++ migrations/20160613195556_peer_observation_standard_success_criteria.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('peer_observation_standard_success_criteria', function(table){
+    table.increments();
+    table.integer('peer_observation_standard_id').unsigned().references('id').inTable('peer_observation_standard').onDelete('cascade');
+    table.integer('success_criteria_id').unsigned().references('id').inTable('success_criteria').onDelete('cascade');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('peer_observation_standard_success_criteria');
+};
